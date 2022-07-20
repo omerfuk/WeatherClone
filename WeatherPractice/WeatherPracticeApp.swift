2c61818745125563e4e5a76e6ed1607c9d83db00
Ömer Faruk Kılıçaslan <omerrr_2001@hotmail.com>
2022-07-20 11:54:38 +0300

    Initial Commit

---
+++ WeatherPractice/WeatherPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherPracticeApp.swift
+//  WeatherPractice
+//
+//  Created by Ömer Faruk Kılıçaslan on 20.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct WeatherPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
